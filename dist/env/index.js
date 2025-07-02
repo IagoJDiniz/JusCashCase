5ff8199cadf4bb0edf45110c15aaf7be91f7fe64
Iago Diniz <iagoo_ptu@hotmail.com>
2025-07-02 13:16:46 -0300

    Revert "build:removendo dist do repositório e adicionando algumas regras no gitignore"
    This reverts commit 74a67f708fd5148b9195001c941d295fe9bd3a7c.

---
+++ dist/env/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.env = void 0;
+require("dotenv/config");
+const zod_1 = require("zod");
+const envSchema = zod_1.z.object({
+    NODE_ENV: zod_1.z.enum(["dev", "test", "production"]).default("dev"),
+    JWT_SECRET: zod_1.z.string(),
+    DATABASE_URL: zod_1.z.string(),
+    BODY_DECRYPTION_KEY: zod_1.z.string(),
+    PORT: zod_1.z.coerce.number().default(3333),
+});
+const _env = envSchema.safeParse(process.env);
+if (_env.success === false) {
+    console.error("❌ Invalid environment variables", _env.error.format());
+    throw new Error("Invalid environment variables.");
+}
+exports.env = _env.data;
