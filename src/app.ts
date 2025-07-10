4c87385dd13205bbc73ca8eed5be1cf03b985166
Iago Diniz <iagoo_ptu@hotmail.com>
2025-07-10 11:01:56 -0300

    fix:reabilitando o cookie parser

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,11 @@ app.use(
 //A quantidade de posts era muito grande, então aumentei o limite de requisição
 app.use(express.json({ limit: "50mb" }));
 
+app.use(cookieParser());
+
 app.use(userRoutes);
 app.use(postRoutes);
 
-app.use(cookieParser());
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
